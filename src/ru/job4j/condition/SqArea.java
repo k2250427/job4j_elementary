1a64352cd911eb060feabbcb643d8a1ed5dd9eb2
Stasyuk.K <kstasyuk@ya.ru>
2021-04-07 17:26:15 +0300

    9.1. String [#512695]: вычисление через три формулы

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -3,7 +3,9 @@ package ru.job4j.condition;
 public class SqArea {
 
     public static double square(int p, double k) {
-        return Math.pow(p / (2 * (k + 1)), 2) * k;
+        double h = p / (2 * (k + 1));
+        double L = h * k;
+        return L * h;
     }
 
     public static void main(String[] args) {
