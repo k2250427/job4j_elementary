d15b98d8d2a83321e6a73d63f1f807dfe3339a1d
Stasyuk.K <kstasyuk@ya.ru>
2021-04-07 14:41:55 +0300

    3.3. Разбор первой программы. [#512705]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Konstantin Stasyuk");
+        System.out.println("31.05.1971");
+    }
+}
