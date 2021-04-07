a7b24ceee97b545b32256277267298f245aaf63d
Stasyuk.K <kstasyuk@ya.ru>
2021-04-07 18:20:35 +0300

    9.1. String [#512695]: вычисление через три формулы

--- src/ru/job4j/condition/Greeting.java
+++ src/ru/job4j/condition/Greeting.java
@@ -3,7 +3,7 @@ package ru.job4j.condition;
 public class Greeting {
 
     public static void main(String[] args) {
-        String idea ="I like Java!";
+        String idea = "I like Java!";
         System.out.println(idea);
         idea = idea + " But I am a newbie.";
         int year = 2021;
