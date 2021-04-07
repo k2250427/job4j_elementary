b51ad4f651f2746b254e98f7c8594f1759bbca83
Stasyuk.K <kstasyuk@ya.ru>
2021-04-07 17:21:41 +0300

    9.1. String [#512695]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,14 @@
+package ru.job4j.condition;
+
+public class Greeting {
+
+    public static void main(String[] args) {
+        String idea ="I like Java!";
+        System.out.println(idea);
+        idea = idea + " But I am a newbie.";
+        int year = 2021;
+        idea = idea + 2021;
+        System.out.println(idea);
+    }
+
+}
